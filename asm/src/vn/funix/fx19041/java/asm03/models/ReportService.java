88452a17a14f2fa78816907b09a50e832e6d5d83
nhat-ninja <115442349+nhat-ninja@users.noreply.github.com>
2023-01-06 13:47:10 +0900

    Delete asm/src/vn/funix/fx19041/java/asm03 directory

--- asm/src/vn/funix/fx19041/java/asm03/models/ReportService.java
+++
@@ -1,5 +0,0 @@
-package vn.funix.fx19041.java.asm03.models;
-
-public interface ReportService {
-    void log(double amount);
-}
--- asm/src/vn/funix/fx19041/java/asm03/models/Withdraw.java
+++
@@ -1,8 +0,0 @@
-package vn.funix.fx19041.java.asm03.models;
-
-public interface Withdraw {
-    double getFee(double amount);
-
-    boolean withdraw(double amount);
-    boolean isAccepted(double amount);
-}
